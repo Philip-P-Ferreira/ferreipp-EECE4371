197624369f914578c697e6058295aad65f80bf80
Philip Ferreira <philip.p.ferreira@vanderbilt.edu>
2020-09-01 22:49:28 -0500

    commented code, updated gitignore

--- ec1/Notes.java
+++ ec1/Notes.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 class Notes {
+    // constants for commands inputs
     public static final String POST_NOTE_STRING = "POST";
     public static final String RETRIEVE_NOTE_STRING = "RETRIEVE";
     public static final String INVALID_COMMAND = "INVALID";
     public static final int POST_COMMAND_SIZE = 3;
     public static final int RETRIEVE_COMMAND_SIZE = 2;
 
+    // enum for creating commands
     public static enum COMMANDS {
         POST_NOTE,
         RETRIEVE_NOTE,
@@ -15,8 +17,10 @@ class Notes {
 
     public static void main(String[] args) {
 
+        // need this to make compiler happy
         boolean run = true;
 
+        // initialize vars to store input, console, and notes
         ArrayList<String> inputArr;
         Scanner console = new Scanner(System.in);
         HashMap<String,String> notesMap = new HashMap<String,String>();
@@ -31,10 +35,12 @@ class Notes {
             // interpret input, perform action based on resulting command
             switch (interpret(inputArr)) {
 
+                // add note to map
                 case POST_NOTE:
                     notesMap.put(inputArr.get(1), inputArr.get(2));
                     break;
 
+                // fetch note from map
                 case RETRIEVE_NOTE:
                     String noteToFetch = inputArr.get(1);
                     if (notesMap.containsKey(noteToFetch)) {
@@ -52,30 +58,48 @@ class Notes {
         }
         console.close();
     }
+    
     public static COMMANDS interpret(final ArrayList<String> inputArr) {
 
+        // check for input command
         switch (inputArr.get(0).toUpperCase()) {
+
             case POST_NOTE_STRING:
                 if (inputArr.size() != POST_COMMAND_SIZE) {
+
                     return COMMANDS.INVALID_COMMAND;
                 }
                 return COMMANDS.POST_NOTE;
+
             case RETRIEVE_NOTE_STRING:
                 if (inputArr.size() != RETRIEVE_COMMAND_SIZE) {
+
                     return COMMANDS.INVALID_COMMAND;
                 }
                 return COMMANDS.RETRIEVE_NOTE;
+
             default:
                 return COMMANDS.INVALID_COMMAND;
         }
     }
 
     public static ArrayList<String> convertInput(final String input) {
+
+        // make a new array list and get first index
         ArrayList<String> returnArr = new ArrayList<String>();
         int firstSpace = input.indexOf(" ");
-        int colonIdx = input.indexOf(":");
 
+        // if no space, return an array with empty string
+        if (firstSpace == -1) {
+            returnArr.add("");
+            return returnArr;
+        }
+
+        // get next index
+        int colonIdx = input.indexOf(":");
         returnArr.add(input.substring(0, firstSpace).trim());
+
+        // add next string based on if index is valid or not
         if (colonIdx != -1) {
             returnArr.add(input.substring(firstSpace, colonIdx).trim());
             returnArr.add(input.substring(colonIdx + 1).trim());
