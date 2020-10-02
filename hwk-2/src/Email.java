15cd345ca0683d8b4d671926b1a29836bd9bc4d3
Philip Ferreira <philip.p.ferreira@vanderbilt.edu>
2020-10-02 13:06:48 -0500

    actually added new files

---
+++ hwk-2/src/Email.java
@@ -0,0 +1,9 @@
+public class Email {
+    public String userField;
+    public String body;
+
+    public Email(String user, String text) {
+        userField = user;
+        body = text;
+    }
+}
