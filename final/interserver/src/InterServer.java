69b3ab1525cfcefc20e2d0a445ebe91629f43bde
Philip Ferreira <philip.p.ferreira@vanderbilt.edu>
2020-11-07 15:41:43 -0600

    made android app project:

---
+++ final/interserver/src/InterServer.java
@@ -0,0 +1,5 @@
+public class InterServer {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ final/storageserver/src/StorageServer.java
@@ -0,0 +1,5 @@
+public class StorageServer {
+    public static void main(String[] args) {
+        
+    }
+}
