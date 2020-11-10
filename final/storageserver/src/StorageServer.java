0bbe294d3a0d59b480baa2afe80ba4f1bb5acc51
Philip Ferreira <philip.p.ferreira@vanderbilt.edu>
2020-11-09 22:41:39 -0600

    started storage server

--- final/storageserver/src/StorageServer.java
+++ final/storageserver/src/StorageServer.java
@@ -1,5 +1,25 @@
+import java.io.*;
+// import java.net.*;
+import java.util.*;
+
+// import commonutils.*;
+
 public class StorageServer {
-    public static void main(String[] args) {
-        
+
+    public static final String STORAGE_PATH = "/mnt/serverstorage/";
+    public static final File STORAGE_PATH_FILE = new File(STORAGE_PATH);
+
+    private static Date lastWriteTime = new Date();
+    private static long totalSpace = STORAGE_PATH_FILE.getTotalSpace();
+    private static long freeSpace = STORAGE_PATH_FILE.getFreeSpace();
+    public static void main(String[] args) throws IOException {
+         
+        System.out.println("\nStarting up Storage Server...");
+        // ServerSocket storageServerSocket = new ServerSocket(ServerProtocol.PORT);
+
+        System.out.println("Free space: " + freeSpace + ", total space: " + totalSpace 
+        + ". Last write: " + lastWriteTime.toString());
+
+        //server always on
     }
 }
