15c204ba5ecbbf0f29ad542a7d33330110d7b436
Philip Ferreira <philip.p.ferreira@vanderbilt.edu>
2020-11-11 12:47:22 -0600

    added cleaner output to storage server

--- final/src/storageserver/StorageServer.java
+++ final/src/storageserver/StorageServer.java
@@ -11,71 +11,113 @@ public class StorageServer {
     public static final String STORAGE_PATH = "iofiles/in/";
     public static final File STORAGE_PATH_FILE = new File(STORAGE_PATH);
 
+    // number constants
+    private static final int SLEEP_TIME = 1000;
+    private static final int REPEAT_COUNT = 4;
+
     // instance tcp stream
     private static TcpStream interStream;
+    private static int attemptCount = 0; // keep track of connect attempts
 
     public static void main(String[] args) throws IOException {
-        
-        // create server socket at PORT
-        System.out.println("\nStarting up Storage Server...");
-        HashMap<String,String> requestMap;
+
+        // vars to keep track of server connection status
+        boolean connectedToServer = false;
+        HashMap<String, String> requestMap;
 
         // server always on
         boolean serverOn = true;
-        boolean connectedToServer = false;
         while (serverOn) {
-            // attempt to connect to to inter server
-            try {
-                System.out.println("Attempting to connect to intermediate server");
-                interStream = new TcpStream(INTERSERVER_ADDRESS, STORAGE_PORT);
-                System.out.println("Connected to intermediate server");
 
-                connectedToServer = true;
-            } catch (IOException e) {
-                System.out.println("Failed to connect to intermediate server");
-            }
+            // method to attempt connect, returns corresponding boolean
+            connectedToServer = connectToInterServer();
 
             // repeat while connection is still active
             while (connectedToServer) {
                 try {
+                    // handle request type
                     requestMap = createProtocolMap(interStream.readMessage(), PAIR_DELIM, PAIR_SEPARATOR);
                     switch (requestMap.get(REQUEST_KEY)) {
-                        
+
                         case UPLOAD_START_VAL:
                             handleUpload(requestMap);
                             break;
-                        
+
                         default:
                             break;
                     }
                 } catch (IOException e) {
-                    System.out.println("Connection to intermediate server broken");
+                    System.out.println(StorageStrings.CONNECTION_BROKEN);
                     connectedToServer = false;
+                    sleep();
                 }
             }
         }
         interStream.close();
     }
 
-    private static void handleUpload(HashMap<String,String> req) throws IOException {
-        
+    // helper method to connect to server, returns corresponding boolean
+    private static boolean connectToInterServer() {
+
+        try {
+            if (attemptCount == 0) {
+                clear_console();
+                System.out.print(StorageStrings.ATTEMPTING_RECONNECT);
+            }
+            interStream = new TcpStream(INTERSERVER_ADDRESS, STORAGE_PORT);
+            
+            // on success, set relevant variables / output
+            attemptCount = 0;
+            System.out.println('\n' + StorageStrings.CONNECTED_TO_INTER);
+            return true;
+
+        } catch (IOException e) {
+            sleep();
+            ++attemptCount;
+
+            // add to output based on how many times we've tried
+            if (attemptCount == REPEAT_COUNT) {
+                clear_console();
+                attemptCount = 0;
+            } else if (attemptCount != 0) {
+                System.out.print('.');
+            }
+            return false;
+        }
+    }
+
+    private static void handleUpload(HashMap<String, String> req) throws IOException {
+
         // get name of file from request
         String filename = STORAGE_PATH + req.get(FILENAME_KEY);
         String filenameZip = filename + ZIP_SUFFIX;
 
         // send ack back
-        HashMap<String,String> res = new HashMap<>();
+        HashMap<String, String> res = new HashMap<>();
         res.put(STATUS_KEY, STATUS_OK_VAL);
         sendProtocolMessage(interStream, UPLOAD_START_ACK_VAL, res);
-        System.out.println("Ready to receive upload...");
+        System.out.println(StorageStrings.UPLOAD_READY);
 
         // write socket in to file
         FileOutputStream outToFile = new FileOutputStream(filenameZip);
         interStream.readToOutputStream(outToFile);
-        
+
         // reconnect to server (file write breaks socket)
         interStream = new TcpStream(INTERSERVER_ADDRESS, STORAGE_PORT);
-        
-        System.out.println("File received, reconnected to server");
+        System.out.println(StorageStrings.UPLOAD_RECEIVED);
+    }
+
+    // helper method to easily clear console
+    private static void clear_console() {
+        System.out.print("\033[H\033[2J");
+    }
+
+    // helper method to sleep execution
+    private static void sleep() {
+        try {
+            Thread.sleep(SLEEP_TIME);
+        } catch (InterruptedException ignored) {
+            // don't care about exception
+        }
     }
 }
---
+++ final/src/storageserver/StorageStrings.java
@@ -0,0 +1,9 @@
+public class StorageStrings {
+
+    public static final String ATTEMPTING_RECONNECT = "Attempting to connect to intermediate server";
+    public static final String CONNECTED_TO_INTER = "Connected to intermediate server";
+    public static final String CONNECTION_BROKEN = "Connection to intermediate server broken";
+    public static final String UPLOAD_READY = "Ready to receive upload...";
+    public static final String UPLOAD_RECEIVED = "File received, reconnected to server";
+    
+}
